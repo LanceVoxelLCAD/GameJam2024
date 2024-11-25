ea0845bcd809e28e88adf53b37ac26622f68faf0
LanceVoxel <lancevoxel@lcad.edu>
2024-11-24 16:19:48 -0800

    Boss Switches Up Attacks

--- GameJam2024/Assets/Scripts/BossAnimator.cs
+++ GameJam2024/Assets/Scripts/BossAnimator.cs
@@ -6,24 +6,34 @@ public class BossAnimator : MonoBehaviour
 {
     public Animator animator;
     public bool isAttacking;
+    public GameObject attackGroup;
+    private float nextShotTime;
+    public float attackTime = 4f;
 
     private void Start()
     {
         isAttacking = animator.GetBool("isAttacking");
+        isAttacking = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && !isAttacking)
+        if (!isAttacking && Time.time > nextShotTime)
         {
             animator.SetBool("isAttacking", true);
             isAttacking = true;
+            attackGroup.SetActive(true);
+            nextShotTime = Time.time + attackTime;
+            //set timer for attack
         }
-        else if (Input.GetKeyDown(KeyCode.E) && isAttacking)
+        else if (isAttacking && Time.time > nextShotTime)
         {
             animator.SetBool("isAttacking", false);
             isAttacking = false;
+            attackGroup.SetActive(false);
+            nextShotTime = Time.time + attackTime;
+            //turn on empty
         }
     }
 }
